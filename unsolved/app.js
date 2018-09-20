d0340c5c4ebf98c9ebcb33dd4f8eb61100e509d6
Jared Smith <jsmith1088@gmail.com>
2018-09-20 09:15:19 -0500

    update

---
+++ unsolved/app.js
@@ -0,0 +1,16 @@
+function employees(){
+    console.log("Hello World")
+    document.getElementById("demo").innerHTML = employeeList[0];
+    console.log(employeeList);
+}
+
+// var cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
+
+// document.addEventListener("click")
+
+document.getElementById("list").addEventListener("click",employeeList);
+
+
+// if (name = ){}
+
